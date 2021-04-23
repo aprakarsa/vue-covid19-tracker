eacb837734e20a76341c337be3c2249bf0df60e6
Aditya Prakarsa <aprakarsa@gmail.com>
2021-04-24 00:33:06 +0800

    first init

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <!-- first box -->
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New: </span>
+                {{ stats.NewConfirmed.toLocaleString() }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total: </span>
+                {{ stats.TotalConfirmed.toLocaleString() }}
+            </div>
+        </div>
+
+        <!-- second box -->
+        <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Recovered</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New: </span>
+                {{ stats.NewRecovered.toLocaleString() }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total: </span>
+                {{ stats.TotalRecovered.toLocaleString() }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataBoxes',
+    props: ['stats'],
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i>Covid-19<i class="fa fa-viruses"></i> Tracker</div>
+  
+  <p>API by <a href="https://covid19api.com" target="_blank">covid19api.com</a><i class="fa fa-viruses"></i></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
